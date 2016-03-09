8ea4e18a55b25cc583d6a1c742b827d49ad2ce58
Andre Askarinam <andreaskarinam@airbears2-10-142-11-181.airbears2.1918.berkeley.edu>
2016-03-08 21:30:49 -0800

    Retrieves location - Needs Google Maps to update first before ApiClient can update

--- mobile/src/main/java/com/example/andreaskarinam/represent/Main_mobile.java
+++ mobile/src/main/java/com/example/andreaskarinam/represent/Main_mobile.java
@@ -1,17 +1,28 @@
 package com.example.andreaskarinam.represent;
 
 import android.content.Intent;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.andreaskarinam.mylibrary.FakeData;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
-public class Main_mobile extends AppCompatActivity {
+public class Main_mobile extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     Button enter_zipcode;
     Button current_location;
+
+    Location mLastLocation;
+    String mLatitudeText;
+    String mLongitudeText;
+
+    GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +31,48 @@ public class Main_mobile extends AppCompatActivity {
 
         enter_zipcode = (Button) findViewById(R.id.button);
         current_location = (Button) findViewById(R.id.button2);
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+    }
+
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onRestart()
+    {  // After a pause OR at startup
+        mGoogleApiClient.connect();
+        super.onRestart();
+    }
+
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        System.out.println("Trying to get last location");
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            System.out.println(mLastLocation.getLatitude());
+            mLatitudeText = String.valueOf(mLastLocation.getLatitude());
+            mLongitudeText = String.valueOf(mLastLocation.getLongitude());
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
     }
 
     public void switch_to_zipcode_view(View view) {
@@ -28,12 +81,23 @@ public class Main_mobile extends AppCompatActivity {
     }
 
     public void switch_to_congressional_view(View view) {
+        String message = "/Latitude and Longitude";
+        String message_contents = mLatitudeText + " " + mLongitudeText;
+        System.out.println(message_contents);
+
         Intent sendIntent = new Intent(getBaseContext(), PhoneToWatchService.class);
-        sendIntent.putExtra(FakeData.COUNTY_INDEX_KEY, 0);
+        sendIntent.putExtra(message, message_contents);
+//        sendIntent.putExtra(FakeData.COUNTY_INDEX_KEY, 0);
         startService(sendIntent);
 
         Intent intent = new Intent(this, congressional_mobile.class);
-        intent.putExtra(FakeData.COUNTY_INDEX_KEY, 0);
+//        intent.putExtra(FakeData.COUNTY_INDEX_KEY, 0);
+        intent.putExtra(message, message_contents);
         startActivity(intent);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 }
--- mobile/src/main/java/com/example/andreaskarinam/represent/zipcode_mobile.java
+++ mobile/src/main/java/com/example/andreaskarinam/represent/zipcode_mobile.java
@@ -3,26 +3,43 @@ package com.example.andreaskarinam.represent;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.andreaskarinam.mylibrary.FakeData;
 
 public class zipcode_mobile extends AppCompatActivity {
 
+    EditText zipcode_text;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zipcode_mobile);
+
+        zipcode_text = (EditText) findViewById(R.id.zipcode_text);
     }
 
     public void switch_to_congressional_view(View view) {
-        Intent sendIntent = new Intent(getBaseContext(), PhoneToWatchService.class);
-        sendIntent.putExtra(FakeData.COUNTY_INDEX_KEY, 1);
-        startService(sendIntent);
+        String message = "/Zipcode";
+        String message_contents = zipcode_text.getText().toString();
 
-        Intent intent = new Intent(this, congressional_mobile.class);
-        intent.putExtra(FakeData.COUNTY_INDEX_KEY, 1);
-        startActivity(intent);
+        // Add code to validate the zipcode entered?
+
+        if (message_contents.length() != 5) {
+            Toast toast = Toast.makeText(getBaseContext(), "Invalid zipcode", Toast.LENGTH_SHORT);
+            toast.show();
+        } else {
+            Intent sendIntent = new Intent(getBaseContext(), PhoneToWatchService.class);
+            sendIntent.putExtra(message, message_contents);
+            startService(sendIntent);
+
+            Intent intent = new Intent(this, congressional_mobile.class);
+            intent.putExtra(message, message_contents);
+            startActivity(intent);
+        }
     }
 }
