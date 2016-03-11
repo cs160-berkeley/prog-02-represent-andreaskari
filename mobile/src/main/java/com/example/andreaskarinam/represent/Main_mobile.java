e2d437bf668f6990262b9c3c32c4501a8af26dfd
Andre Askarinam <andreaskarinam@airbears2-10-142-8-172.airbears2.1918.berkeley.edu>
2016-03-10 23:34:51 -0800

    Got basic basic Twitter working

--- mobile/src/main/java/com/example/andreaskarinam/represent/Main_mobile.java
+++ mobile/src/main/java/com/example/andreaskarinam/represent/Main_mobile.java
@@ -4,16 +4,32 @@ import android.content.Intent;
 import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.example.andreaskarinam.mylibrary.FakeData;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.models.Tweet;
+import com.twitter.sdk.android.tweetui.TweetUtils;
+import com.twitter.sdk.android.tweetui.TweetView;
+
+import io.fabric.sdk.android.Fabric;
 
 public class Main_mobile extends AppCompatActivity implements
         GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "7sIEH0sd2ieymR8e9i9HV3BnE";
+    private static final String TWITTER_SECRET = "WhWUsVkDiLFTJCpM3wDwf2VsaxQ4GptKPogbBHA4mKPXykYlo2";
 
     Button enter_zipcode;
     Button current_location;
@@ -27,6 +43,8 @@ public class Main_mobile extends AppCompatActivity implements
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_main_mobile);
 
         enter_zipcode = (Button) findViewById(R.id.button);
@@ -39,6 +57,24 @@ public class Main_mobile extends AppCompatActivity implements
                     .addApi(LocationServices.API)
                     .build();
         }
+
+        // TODO: Use a more specific parent
+        final ViewGroup parentView = (ViewGroup) getWindow().getDecorView().getRootView();
+        // TODO: Base this Tweet ID on some data from elsewhere in your app
+        long tweetId = 631879971628183552L;
+        TweetUtils.loadTweet(tweetId, new Callback<Tweet>() {
+            @Override
+            public void success(Result<Tweet> result) {
+                TweetView tweetView = new TweetView(Main_mobile.this, result.data);
+                parentView.addView(tweetView);
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                Log.d("TwitterKit", "Load Tweet failure", exception);
+            }
+        });
+
     }
 
     protected void onStart() {
